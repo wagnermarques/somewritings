f31c4f4ce50d1c3016dc0a1f6d841aa4cf147b1f
root <root@localhost.localdomain>
2017-07-14 01:30:28 -0300

    Trabalhando material de aulas pra etec

---
+++ typescript-introduction/examples/ex1/greeter.ts
@@ -0,0 +1,8 @@
+//http://www.diullei.com/TypeScript-ptBR/tutorial.html
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+var user = "Jane User";
+
+document.body.innerHTML = greeter(user);
