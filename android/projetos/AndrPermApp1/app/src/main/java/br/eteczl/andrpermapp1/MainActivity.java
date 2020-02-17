7baf64785cd073712b3e17822e7f79cc54c0c782
wagnermarques <wagnerdocri@gmail.com>
2020-02-17 14:24:44 -0300

    several changes

--- android/projetos/AndrPermApp1/app/src/main/java/br/eteczl/andrpermapp1/MainActivity.java
+++ android/projetos/AndrPermApp1/app/src/main/java/br/eteczl/andrpermapp1/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import static android.Manifest.permission.ACCESS_BACKGROUND_LOCATION;
@@ -20,92 +21,77 @@ public class MainActivity extends AppCompatActivity {
 
     private final int ALL_PERMISSIONS_RESULT = 101;
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        boolean ANTES_PermACCESS_COARSE_LOCATION = this.checkPermissionIsGranted(this, ACCESS_COARSE_LOCATION);
-        Toast.makeText(this,"ANTES_PermACCESS_COARSE_LOCATION : "+ANTES_PermACCESS_COARSE_LOCATION,Toast.LENGTH_SHORT).show();
-
-        boolean ANTES_PermACCESS_FINE_LOCATION = this.checkPermissionIsGranted(this, ACCESS_FINE_LOCATION);
-        Toast.makeText(this,"ANTES_PermACCESS_FINE_LOCATION : "+ANTES_PermACCESS_FINE_LOCATION,Toast.LENGTH_SHORT).show();
-
-        boolean ANTES_PermACCESS_BACKGROUND_LOCATION = this.checkPermissionIsGranted(this, ACCESS_BACKGROUND_LOCATION);
-        Toast.makeText(this,"ANTES_PermACCESS_BACKGROUND_LOCATION : "+ANTES_PermACCESS_BACKGROUND_LOCATION,Toast.LENGTH_SHORT).show();
-
-        askUserForPermission(ACCESS_COARSE_LOCATION);
-        askUserForPermission(ACCESS_FINE_LOCATION);
-        askUserForPermission(ACCESS_BACKGROUND_LOCATION);
-
-        boolean Depois_ANTES_PermACCESS_COARSE_LOCATION = this.checkPermissionIsGranted(this, ACCESS_COARSE_LOCATION);
-        Toast.makeText(this,"Depois_ANTES_PermACCESS_COARSE_LOCATION : "+Depois_ANTES_PermACCESS_COARSE_LOCATION,Toast.LENGTH_SHORT).show();
-
-        boolean Depois_ANTES_PermACCESS_FINE_LOCATION = this.checkPermissionIsGranted(this, ACCESS_FINE_LOCATION);
-        Toast.makeText(this,"Depois_ANTES_PermACCESS_FINE_LOCATION : "+Depois_ANTES_PermACCESS_FINE_LOCATION,Toast.LENGTH_SHORT).show();
-
-        boolean Depois_ANTES_PermACCESS_BACKGROUND_LOCATION = this.checkPermissionIsGranted(this, ACCESS_BACKGROUND_LOCATION);
-        Toast.makeText(this,"Depois_ANTES_PermACCESS_BACKGROUND_LOCATION : "+Depois_ANTES_PermACCESS_BACKGROUND_LOCATION,Toast.LENGTH_SHORT).show();
-
+        View btnPedePermissao = findViewById(R.id.btnPedePermissao);
+        btnPedePermissao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.solicitaPermissaoDeLocation();
+            }
+        });
     }
+
+
 
 
     //antes do lollipop a administracao das permissoes era na instalacao
     //era install time e nao como hoje, no runtime time (tempo de execucao)
     //por isso a gente precisa saber em qual plataforma android nosso software
     //esta rodando
-    private static boolean platformIsAboveLollipop() {
+    private boolean platformIsAboveLollipop() {
         return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
     }
 
 
+    public boolean permissaoConcedida(String perm) {
 
-    public boolean checkPermissionIsGranted(MainActivity mainActivity, String perm) {
-        //se for lollipo so o fato do app estar instalado
-        //significa que a permissao ja foi concedido caso contrario
-        //a instalacao nem ocorreria
-        //entao so faz sentido checar se a plat for
-        //above lollipop
         if (platformIsAboveLollipop()) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                return (
-                        ContextCompat.checkSelfPermission(this,
-                                perm) == PackageManager.PERMISSION_GRANTED);
+                //Toast.makeText(this,"PackageManager.PERMISSION_GRANTED = "+PackageManager.PERMISSION_GRANTED, Toast.LENGTH_SHORT).show();
+                return (ContextCompat.checkSelfPermission(this, perm) == PackageManager.PERMISSION_GRANTED);
             }
         }
         return true;
     }
 
 
+    public void solicitaPermissaoDeLocation(){
+        Toast.makeText(this,"public void solicitaPermissaoDeLocation(){...", Toast.LENGTH_SHORT).show();
+        askUserForPermission(ACCESS_COARSE_LOCATION);
+    }
 
     private void askUserForPermission(String perm){
         //Vamos perguntar pro usuario apenas permissoes que
         //ainda nao foram concedidas
-        if (!this.checkPermissionIsGranted(this, perm)) {
+        if (!this.permissaoConcedida(perm)) {
 
             // Permission is not granted
             // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    perm)) {
+            //Toast.makeText(this, "Permission "+perm+ "###AINDA NAO FOI concedida anteriormente" , Toast.LENGTH_LONG).show();
+
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, perm)) {
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
-            } else {
+//            } else {
                 // No explanation needed; request the permission
 
-                ActivityCompat.requestPermissions(this,
-                        new String[]{
-                                perm},
-                        ALL_PERMISSIONS_RESULT);
+                ActivityCompat.requestPermissions(this, new String[]{perm}, ALL_PERMISSIONS_RESULT);
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
                 // result of the request.
-            }
+//            }
         } else {
             // Permission has already been granted
-            Toast.makeText(this, "Permission ACCESS_COARSE_LOCATION has already been granted" , Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "PERMISSAO "+perm+ " ja foi concedida anteriormente" , Toast.LENGTH_LONG).show();
         }
     }
 }
