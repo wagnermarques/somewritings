12ba498c7b270c4df3bfe0ca1b3582551d4b4ef7
wagnermarques <wagnerdocri@gmail.com>
2022-08-02 07:46:43 -0300

    subjects lisp, js, aulas de cordova added

---
+++ javascript/PushAPI/pwapushapi/web/startPhpServer.sh
@@ -0,0 +1,10 @@
+export apiKey="AIzaSyB240-iOsNvzyuJKsMczQlmCFnJ1C0MHg4"
+export authDomain="fzlbpms-8115f.firebaseapp.com" 
+export databaseURL="fzlbpms-8115f"
+export projectId="fzlbpms-8115f"
+export storageBucket="fzlbpms-8115f.appspot.com"
+export messagingSenderId="393177754483"
+export appId="1:393177754483:web:f088f5f690ad1e9502b306"
+pastaDoMeuProjeto=/home/administrador/fzlbpms/submodules/somewritings/javascript/PushAPI/pwapushapi/web
+cd $pastaDoMeuProjeto
+php -S localhost:3000
